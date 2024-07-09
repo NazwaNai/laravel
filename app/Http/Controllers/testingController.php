4f225f336fc11dc9d49d7edd76b80b9e57c2009e
NazwaNai <nazwazivana750@gmail.com>
2024-07-09 10:40:56 +0700

    laravel

---
+++ app/Http/Controllers/testingController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Controllers\testingController;
+use Illuminate\Http\Request;
+
+class testingController extends Controller
+{
+    public function testing (){
+        return 'INI CONTROLLER TESTING';
+    }
+}
